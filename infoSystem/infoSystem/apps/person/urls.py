13fd137eb78fa74f165859ac768a8804d600a2c2
Freeroed <Telekines1551@gmail.com>
2020-06-11 16:58:11 +0300

    Реализована страница добавления человека

--- infoSystem/infoSystem/apps/person/urls.py
+++ infoSystem/infoSystem/apps/person/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'persons'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('test/', views.test, name='test')
+    path('test/', views.test, name='test'),
+    path('create/', views.create_page, name='create_page'),
+    path('add/', views.create, name='create')
 ]
--- infoSystem/infoSystem/apps/person/views.py
+++ infoSystem/infoSystem/apps/person/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Person
 
 def index(request):
@@ -10,3 +11,15 @@ def index(request):
 
 def test(request):
     return render(request, 'person/test.html')
+
+def create_page(request):
+    return render(request, 'person/create.html')
+
+def create(request):
+    Person.objects.create(
+        name = request.POST['name'],
+        surmane = request.POST['surmane'],
+        birthdate = request.POST['birthdate'],
+        image_url = request.POST['image_url']
+    )
+    return HttpResponseRedirect( reverse('persons:index'))
